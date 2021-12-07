650b8ea07a4ed322772a69b3337a7ac5c4762674
sg3-141-592 <sean@gilligan.me>
2021-12-07 05:59:08 +0000

    Added some autoformatting

--- backend/app.py
+++ backend/app.py
@@ -1,109 +1,140 @@
 from flask import Flask, request, json, Response
-from flask_socketio import SocketIO, join_room, leave_room, emit, rooms
+from flask_socketio import SocketIO, join_room, emit
 import logging
 import random_name
 import database
 
 import eventlet
+
 eventlet.monkey_patch()
 
-defaultLogger = logging.basicConfig(filename='errors.log', level=logging.INFO)
+defaultLogger = logging.basicConfig(filename="errors.log", level=logging.INFO)
 
 app = Flask(__name__)
-socketio = SocketIO(app, cors_allowed_origins="*", async_mode='eventlet')
+socketio = SocketIO(app, cors_allowed_origins="*", async_mode="eventlet")
 
-URL_SUBFOLDER = '/api'
+URL_SUBFOLDER = "/api"
 
-@app.route(URL_SUBFOLDER+'/createPoll', methods=['POST'])
+"""
+Create new poll
+"""
+@app.route(URL_SUBFOLDER + "/createPoll", methods=["POST"])
 def createPoll():
     data = request.json
     publicId = random_name.generate_name()
-    
+
     newPoll = database.Poll(
-        name = data['pollName'],
-        options = {
-            'one': data['optionOne'],
-            'oneEmoji': data['optionOneEmoji'],
-            'two': data['optionTwo'],
-            'twoEmoji': data['optionTwoEmoji'],
+        name=data["pollName"],
+        options={
+            "one": data["optionOne"],
+            "oneEmoji": data["optionOneEmoji"],
+            "two": data["optionTwo"],
+            "twoEmoji": data["optionTwoEmoji"],
         },
-        userId = data['userId'],
-        publicId = publicId
+        userId=data["userId"],
+        publicId=publicId,
     )
 
     database.session.add(newPoll)
     database.session.commit()
-    
-    return Response(json.dumps({
-            'id': publicId
-        }),
-        status=200,
-        mimetype='application/json'
+
+    return Response(
+        json.dumps({"id": publicId}), status=200, mimetype="application/json"
     )
+
 
 """
 Get a list of polls for a particular user
 """
-@app.route(URL_SUBFOLDER+'/getPolls', methods=['GET'])
+@app.route(URL_SUBFOLDER + "/getPolls", methods=["GET"])
 def getPolls():
-    userId = request.args.get('userId')
+    userId = request.args.get("userId")
     print(userId)
-    polls = database.session.query(database.Poll).filter_by(userId=userId).order_by(database.Poll.created.desc())
+    polls = (
+        database.session.query(database.Poll)
+        .filter_by(userId=userId)
+        .order_by(database.Poll.created.desc())
+    )
     pollList = []
     for pollObject in polls:
-        pollList.append({
-            'name': pollObject.name,
-            'url': pollObject.publicId
-        })
-    return Response(
-        json.dumps(pollList),
-        status=200,
-        mimetype='application/json'
-    )
+        pollList.append({"name": pollObject.name, "url": pollObject.publicId})
+    return Response(json.dumps(pollList), status=200, mimetype="application/json")
 
-@socketio.on('connect')
+
+@socketio.on("connect")
 def get_connect():
     print("Client connected")
+
 
 def getPollData(pollId, userId):
     # Check if user has any votes for poll
     latestVote = 0.5
-    latestVoteQuery = database.session.query(database.Vote).\
-        filter_by(pollId=pollId).\
-        filter_by(userId=userId).order_by(database.Vote.created).first()
+    latestVoteQuery = (
+        database.session.query(database.Vote)
+        .filter_by(pollId=pollId)
+        .filter_by(userId=userId)
+        .order_by(database.Vote.created)
+        .first()
+    )
     if latestVoteQuery:
         latestVote = latestVoteQuery.value
     #
     pollObject = database.session.query(database.Poll).filter_by(publicId=pollId).one()
     pollJson = {
-        'name': pollObject.name,
-        'options': pollObject.options,
-        'latestVote': latestVote,
+        "name": pollObject.name,
+        "options": pollObject.options,
+        "latestVote": latestVote,
     }
     return pollJson
 
-@socketio.on('vote')
+
+@socketio.on("vote")
 def vote(data):
     newVote = database.Vote(
-        value = data['value'],
-        pollId = data['pollId'],
-        userId = data['userId']
+        value=data["value"], pollId=data["pollId"], userId=data["userId"]
     )
     database.session.add(newVote)
     database.session.commit()
-    
-    print("Vote update")
-    emit('updateVotingDetails', database.getLatestVotes(data['pollId']), room=data['pollId'], json=True)
-    emit('updateAnalyticsDetails', database.getAverageVoteData(data['pollId']), room=data['pollId'], json=True)
 
-@socketio.on('join')
+    print("Vote update")
+    emit(
+        "updateVotingDetails",
+        database.getLatestVotes(data["pollId"]),
+        room=data["pollId"],
+        json=True,
+    )
+    emit(
+        "updateAnalyticsDetails",
+        database.getAverageVoteData(data["pollId"]),
+        room=data["pollId"],
+        json=True,
+    )
+
+
+@socketio.on("join")
 def on_join(data):
-    pollId = data['pollId']
+    pollId = data["pollId"]
     join_room(pollId)
     currentUser = request.sid
-    emit('updatePollDetails', getPollData(pollId, data['userId']), to=currentUser, json=True)
-    emit('updateVotingDetails', database.getLatestVotes(pollId), to=currentUser, json=True)
-    emit('updateAnalyticsDetails', database.getAverageVoteData(pollId), to=currentUser, json=True)
+    emit(
+        "updatePollDetails",
+        getPollData(pollId, data["userId"]),
+        to=currentUser,
+        json=True,
+    )
+    emit(
+        "updateVotingDetails",
+        database.getLatestVotes(pollId),
+        to=currentUser,
+        json=True,
+    )
+    emit(
+        "updateAnalyticsDetails",
+        database.getAverageVoteData(pollId),
+        to=currentUser,
+        json=True,
+    )
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     socketio.run(app, debug=True, log_output=True)
--- backend/database.py
+++ backend/database.py
@@ -1,4 +1,15 @@
-from sqlalchemy import create_engine, Column, String, Integer, JSON, DateTime, ForeignKey, Float, func, and_
+from sqlalchemy import (
+    create_engine,
+    Column,
+    String,
+    Integer,
+    JSON,
+    DateTime,
+    ForeignKey,
+    Float,
+    func,
+    and_,
+)
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.sql import func
@@ -7,28 +18,35 @@ import datetime
 import pandas as pd
 import numpy as np
 
-engine = create_engine('sqlite:///database/data.db', connect_args={"check_same_thread": False}, echo=False)
+engine = create_engine(
+    "sqlite:///database/data.db", connect_args={"check_same_thread": False}, echo=False
+)
 
 Base = declarative_base()
 
+
 class Poll(Base):
-    __tablename__ = 'polls'
+    __tablename__ = "polls"
 
     id = Column(Integer, primary_key=True)
     name = Column(String)
     options = Column(JSON)
     userId = Column(String, index=True)
-    created = Column(DateTime(timezone=True), default=datetime.datetime.utcnow, index=True)
+    created = Column(
+        DateTime(timezone=True), default=datetime.datetime.utcnow, index=True
+    )
     publicId = Column(String, index=True)
 
+
 class Vote(Base):
-    __tablename__ = 'votes'
+    __tablename__ = "votes"
 
     id = Column(Integer, primary_key=True)
     value = Column(Float)
-    pollId = Column(Integer, ForeignKey('polls.publicId'), index=True)
+    pollId = Column(Integer, ForeignKey("polls.publicId"), index=True)
     userId = Column(String, index=True)
     created = Column(DateTime(timezone=True), default=datetime.datetime.utcnow)
+
 
 Base.metadata.create_all(engine)
 
@@ -36,9 +54,11 @@ Base.metadata.create_all(engine)
 Session = sessionmaker(bind=engine)
 session = Session()
 
+
 def getResamplingInterval(interval):
     # TODO: Add some proper rounding behaviour here
-    return interval/10
+    return interval / 20
+
 
 """
 Get the votes over time for the poll
@@ -47,68 +67,60 @@ def getAverageVoteData(pollId):
     averageData = []
     latestVote = {}
 
-    for item in session.query(Vote.userId, Vote.value, Vote.created).filter_by(pollId=pollId):
+    for item in session.query(Vote.userId, Vote.value, Vote.created).filter_by(
+        pollId=pollId
+    ):
         latestVote[item[0]] = item[1]
         average = 0
         for user in latestVote.keys():
             average += latestVote[user]
         average /= len(latestVote.keys())
-        averageData.append({
-            'y': average,
-            'x': item[2]
-        })
-    
+        averageData.append({"y": average, "x": item[2]})
+
     if len(averageData) == 0:
-        return [{
-            'x': datetime.datetime.utcnow().timestamp(),
-            'y': 0
-        }]
-    
+        return [{"x": datetime.datetime.utcnow().timestamp(), "y": 0.5}]
+
     # Resample data
     df = pd.DataFrame(averageData)
-    
+
     # Get the length of our dataset to work out resampling interval
     interval = df["x"].max() - df["x"].min()
     print("Interval", interval)
     # Add handling for an interval of 1
     if interval <= datetime.timedelta(seconds=1):
         return {
-            'x': datetime.datetime.timestamp(df["x"][0]),
-            'y': df["y"][0],
+            "x": datetime.datetime.timestamp(df["x"][0]),
+            "y": df["y"][0],
         }
-    
-    df = df.set_index('x')
+
+    df = df.set_index("x")
     print("Resampling interval", getResamplingInterval(interval))
-    resampled = df.resample(getResamplingInterval(interval), label='right').mean().dropna()
+    resampled = (
+        df.resample(getResamplingInterval(interval), label="right").mean().dropna()
+    )
     outputData = []
     for index, row in resampled.iterrows():
-        outputData.append({
-            'x' : datetime.datetime.timestamp(index),
-            'y' : row['y']
-        })
-    
+        outputData.append({"x": datetime.datetime.timestamp(index), "y": row["y"]})
+
     return outputData
 
+
 """
-Get the latest votes for all of the users
+Get the latest votes for all of the users on a poll
 """
 def getLatestVotes(pollId):
 
     subq = (
-        session
-        .query(Vote.id, Vote.userId, func.max(Vote.created).label("max_created"))
+        session.query(Vote.id, Vote.userId, func.max(Vote.created).label("max_created"))
         .filter(Vote.pollId == pollId)
         .group_by(Vote.userId)
         .subquery()
     )
 
-    q = (
-        session.query(Vote)
-        .join(subq, subq.c.id == Vote.id)
-    )
+    q = session.query(Vote).join(subq, subq.c.id == Vote.id)
 
     votes = []
     for item in q:
         votes.append(item.value)
-    
+
     return votes
