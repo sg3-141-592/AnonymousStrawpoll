d5d1c872a8b19a5d6ff3573218e1b7c7a3da0639
Sean G <sean@pc.com>
2022-01-31 10:58:19 +0000

    Initial ARM templates

---
+++ deployment/getListOfDBUsers.sql
@@ -0,0 +1,10 @@
+select name as username,
+       create_date,
+       modify_date,
+       type_desc as type,
+       authentication_type_desc as authentication_type
+from sys.database_principals
+where type not in ('A', 'G', 'R', 'X')
+      and sid is not null
+      and name != 'guest'
+order by username;
